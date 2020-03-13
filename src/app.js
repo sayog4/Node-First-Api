23a32bf4fc2cd37de6153a978946c58449239f9e
prokarki <sayogkarki@gmail.com>
2020-03-13 19:02:23 +0530

    CORS ADDED

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ app.use( express.json() )
 app.use((req, res, next) => {
   res.setHeader( 'Access-Control-Allow-Origin', '*' )
   res.setHeader( 'Access-Control-Allow-Methods', 'GET','POST','PATCH','DELETE' )
-  res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type', 'Authorization' )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization')
+  
+  next()
 } )
 app.use(userRouter, taskRouter)
 
