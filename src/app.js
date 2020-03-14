2f60b7e00fd85b5b7d4ea1b656c28721716ce0db
prokarki <sayogkarki@gmail.com>
2020-03-14 13:34:52 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use( express.json() )
 
 app.use((req, res, next) => {
   res.setHeader( 'Access-Control-Allow-Origin', '*' )
-  res.setHeader( 'Access-Control-Allow-Methods', 'GET','POST','PATCH','DELETE' )
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization')
+  res.setHeader( 'Access-Control-Allow-Methods', 'GET ,POST ,PATCH, DELETE' )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   
   next()
 } )
